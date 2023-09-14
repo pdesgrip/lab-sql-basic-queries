d268befe873004abf21bb1c77c3aadc62c53411a
Paul Desgrippes <pdesgrip@gmail.com>
2023-09-14 17:44:51 +0200

    version1

---
+++ SQL_Lab_Solution_W2_D3.sql
@@ -0,0 +1,15 @@
+-- Show all tables
+
+SHOW tables;
+
+-- Retrieve all the data from the tables actor, film and customer
+
+SELECT * FROM actor;
+SELECT * FROM film;
+SELECT * FROM customer;
+
+-- Retrieve the following columns from their respective tables
+
+-- Titles of all films from the film table
+
+SELECT title FROM film;
